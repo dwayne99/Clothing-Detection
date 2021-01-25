f938f4c1fa19761efd030c4068026aa7ed225f07
dwaye99 <dwayne.ferns16@gmail.com>
2021-01-25 22:53:00 +0530

    added focus function

--- new_image_demo.py
+++ new_image_demo.py
@@ -8,7 +8,19 @@ import glob
 from tqdm import tqdm
 import sys
 
+from PIL import Image, ImageFilter
 
+from pdb import set_trace
+
+def focus(img_path,x1,y1,x2,y2):
+    img = Image.open(img_path)
+    # crop the ROI
+    crop = img.crop((x1,y1,x2,y2))
+    # blur the original img
+    img = img.filter(ImageFilter.GaussianBlur(10))
+    # add the focused img to the blur
+    img.paste(crop,(x1,y1,x2,y2))
+    img.show()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.cuda.empty_cache()
@@ -65,67 +77,67 @@ else:
 
 #Faster RCNN / RetinaNet / Mask RCNN
 
-# while(True):
+while(True):
 
 
-#     path = input('img path: ')
-#     if not os.path.exists(path):
-#         print('Img does not exists..')
-#         continue
-#     img = cv2.imread(path)
-#     detections = detectron.get_detections(img)
-#     #detections = yolo.get_detections(img)
-#     #print(detections)
-
-    
-
-#     #unique_labels = np.array(list(set([det[-1] for det in detections])))
-
-#     #n_cls_preds = len(unique_labels)
-#     #bbox_colors = colors[:n_cls_preds]
+    path = input('img path: ')
+    if not os.path.exists(path):
+        print('Img does not exists..')
+        continue
+    img = cv2.imread(path)
+    detections = detectron.get_detections(img)
+    #detections = yolo.get_detections(img)
+    #print(detections)
 
     
-#     if len(detections) != 0 :
-#         detections.sort(reverse=False ,key = lambda x:x[4])
-#         for x1, y1, x2, y2, cls_conf, cls_pred in detections:
-                
-#                 #feat_vec =detectron.compute_features_from_bbox(img,[(x1, y1, x2, y2)])
-#                 #feat_vec = detectron.extract_encoding_features(img)
-#                 #print(feat_vec)
-#                 #print(a.get_field('features')[0].shape)
-#                 print("\t+ Label: %s, Conf: %.5f" % (classes[int(cls_pred)], cls_conf))           
+
+    #unique_labels = np.array(list(set([det[-1] for det in detections])))
+
+    #n_cls_preds = len(unique_labels)
+    #bbox_colors = colors[:n_cls_preds]
+
+    
+    if len(detections) != 0 :
+        detections.sort(reverse=False ,key = lambda x:x[4])
+        for x1, y1, x2, y2, cls_conf, cls_pred in detections:
+                #feat_vec =detectron.compute_features_from_bbox(img,[(x1, y1, x2, y2)])
+                #feat_vec = detectron.extract_encoding_features(img)
+                #print(feat_vec)
+                #print(a.get_field('features')[0].shape)
+                print("\t+ Label: %s, Conf: %.5f" % (classes[int(cls_pred)], cls_conf))           
 
                 
-#                 #color = bbox_colors[np.where(unique_labels == cls_pred)[0]][0]
-#                 color = colors[int(cls_pred)]
+                #color = bbox_colors[np.where(unique_labels == cls_pred)[0]][0]
+                color = colors[int(cls_pred)]
                 
-#                 color = tuple(c*255 for c in color)
-#                 color = (.7*color[2],.7*color[1],.7*color[0])       
+                color = tuple(c*255 for c in color)
+                color = (.7*color[2],.7*color[1],.7*color[0])       
                     
-#                 font = cv2.FONT_HERSHEY_SIMPLEX   
+                font = cv2.FONT_HERSHEY_SIMPLEX   
             
             
-#                 x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-#                 text =  "%s conf: %.3f" % (classes[int(cls_pred)] ,cls_conf)
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                text =  "%s conf: %.3f" % (classes[int(cls_pred)] ,cls_conf)
                 
-#                 cv2.rectangle(img,(x1,y1) , (x2,y2) , color,3)
-#                 y1 = 0 if y1<0 else y1
-#                 y1_rect = y1-25
-#                 y1_text = y1-5
+                cv2.rectangle(img,(x1,y1) , (x2,y2) , color,3)
+                y1 = 0 if y1<0 else y1
+                y1_rect = y1-25
+                y1_text = y1-5
 
-#                 if y1_rect<0:
-#                     y1_rect = y1+27
-#                     y1_text = y1+20
-#                 cv2.rectangle(img,(x1-2,y1_rect) , (x1 + int(8.5*len(text)),y1) , color,-1)
-#                 cv2.putText(img,text,(x1,y1_text), font, 0.5,(255,255,255),1,cv2.LINE_AA)
+                if y1_rect<0:
+                    y1_rect = y1+27
+                    y1_text = y1+20
+                cv2.rectangle(img,(x1-2,y1_rect) , (x1 + int(8.5*len(text)),y1) , color,-1)
+                cv2.putText(img,text,(x1,y1_text), font, 0.5,(255,255,255),1,cv2.LINE_AA)
+                focus(path,x1,y1,x2,y2)
                 
                 
 
                 
-#     # cv2.imshow('Detections',img)
-#     img_id = path.split('/')[-1].split('.')[0]
-#     cv2.imwrite('output/ouput-test_{}_{}_{}.jpg'.format(img_id,model,dataset),img)
-#     # cv2.waitKey(0)
+    # cv2.imshow('Detections',img)
+    img_id = path.split('/')[-1].split('.')[0]
+    cv2.imwrite('output/ouput-test_{}_{}_{}.jpg'.format(img_id,model,dataset),img)
+    # cv2.waitKey(0)
 
 
 # batch process image by detecting all clothing items from multiple images
@@ -176,11 +188,12 @@ def batch_process(dir_path):
         cv2.imwrite(out_img,img)
 
 
-while True:
-    dir_path = input('Enter the folder path: ')
-    if not os.path.exists(dir_path):
-        print('Folder does not exist')
-        continue
-    else:
-        break
-batch_process(dir_path)
+
+# while True:
+#     dir_path = input('Enter the folder path: ')
+#     if not os.path.exists(dir_path):
+#         print('Folder does not exist')
+#         continue
+#     else:
+#         break
+# batch_process(dir_path)
